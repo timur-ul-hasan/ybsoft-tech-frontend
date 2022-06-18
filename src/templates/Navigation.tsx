1a1857ca95e9398fcb6e2a7efb44b21c276fb59f
Hasan Javed <hasanjaved065@gmail.com>
2022-06-18 22:08:36 +0500

    Added Contact Page.

---
+++ src/templates/Navigation.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+import { Background } from '../background/Background';
+import { Section } from '../layout/Section';
+import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
+import { Logo } from './Logo';
+
+const Navigation = () => (
+  <Background color="bg-gray-100">
+    <Section yPadding="py-6">
+      <NavbarTwoColumns logo={<Logo xl />}>
+        <li>
+          <Link href="/contact">
+            <a>Contact</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Sign in</a>
+          </Link>
+        </li>
+      </NavbarTwoColumns>
+    </Section>
+  </Background>
+);
+
+export { Navigation };
